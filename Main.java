97c75b926c27138e7e31b0a6e7fef8cf3e03f576
Izzy Stradlin <izzy.stradlin@outlook.com>
2025-11-10 09:55:45 -0300

    adicionado nova frase e retirado duplicatas.

--- Main.java
+++ Main.java
@@ -4,7 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Aula de GIT");
 		System.out.println("adicionando um teste.");
-		System.out.println("adicionando um teste.");
-		System.out.println("adicionando um teste.");
+		System.out.println("informação extra");
 	}
 }
