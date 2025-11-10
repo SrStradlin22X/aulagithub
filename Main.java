ed005a1cc8cd2761d48a4a3a2badf820bf43c787
Izzy Stradlin <izzy.stradlin@outlook.com>
2025-11-10 09:22:58 -0300

    teste

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Aula de GIT");
 		System.out.println("adicionando um teste.");
+		System.out.println("adicionando um teste.");
+		System.out.println("adicionando um teste.");
 
 	}
 
