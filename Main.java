2d9403bcb248d3bc811aed3f283018b94185ebc3
Izzy Stradlin <izzy.stradlin@outlook.com>
2025-11-10 12:24:29 -0300

    Deletado frases

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Aula de GIT");
-		System.out.println("adicionando um teste.");
-		System.out.println("informação extra");
+		
 	}
 }
